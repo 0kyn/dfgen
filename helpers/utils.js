2ec8e93f3e93653bd2fe7a9f4d9ed8d3639742ed
0kyn <0kyn.dev@gmail.com>
2021-10-26 17:51:08 +0200

    refactor: use static files as db

--- helpers/utils.js
+++ helpers/utils.js
@@ -10,12 +10,7 @@ module.exports.exit = (type, message, code = 1) => {
 }
 
 module.exports.getMimeType = (fileType) => {
-    const mimeType = {
-        pdf: 'application/pdf',
-        jpg: 'image/jpeg',
-        png: 'image/png',
-        gif: 'image/gif'
-    }
+    const mimeType = require('../data/mime-type.json')
 
     if (typeof mimeType[fileType] === 'undefined') {
         this.exit('error', `'${fileType}' mime type is not supported`)
@@ -38,13 +33,7 @@ module.exports.setOutputFilename = (prefix, extension) => {
 }
 
 module.exports.getFileSignature = (fileType) => {
-    const fileSignature = {
-        invalid: [0, 1, 2, 3],
-        pdf: [0x25, 0x50, 0x44, 0x46],
-        jpg: [0xFF, 0xD8, 0xFF],
-        png: [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A],
-        gif: [0x47, 0x49, 0x46]
-    }
+    const fileSignature = require('../data/file-signature.json')
 
     if (typeof fileSignature[fileType] === 'undefined') {
         this.exit('warning', `'${fileType}' signature is not supported. Cannot corrupt file.`)
